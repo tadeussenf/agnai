8f4776c027bfe08473d9fc673024d6df97d1f7af
christoph.strauss <christoph.strauss@flowable.com>
2026-03-14 15:35:24 +0100

    hardcode agnai.chat prod baseUrl

--- web/store/api.ts
+++ web/store/api.ts
@@ -26,21 +26,9 @@ if (location.search) {
   }
 }
 
-const API_OVERRIDE = localStorage.getItem('api_url')
+// const API_OVERRIDE = localStorage.getItem('api_url')
 
-export const baseUrl = API_OVERRIDE
-  ? `${PROTO}//${API_OVERRIDE}`
-  : PORT === '1234' || PORT === '3001' // || HOST === 'localhost' || HOST === '127.0.0.1'
-  ? `${PROTO}//${HOST}:3001`
-  : HOST === 'agnai.chat'
-  ? `${PROTO}//prd-api.agnai.chat`
-  : HOST === 'prd-assets.agnai.chat'
-  ? `${PROTO}//edge-api.agnai.chat`
-  : HOST === 'dev.agnai.chat' || HOST === 'dev-assets.agnai.chat'
-  ? `${PROTO}//edge-api.agnai.chat`
-  : HOST === 'stg.agnai.chat'
-  ? `${PROTO}//stg-api.agnai.chat`
-  : location.origin
+export const baseUrl = 'https://prd-api.agnai.chat';
 
 export const api = {
   get,
